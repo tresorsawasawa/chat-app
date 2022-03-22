6da52a479730a1733df7a136ed51e97bf6fc4d1b
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-22 22:42:00 +0200

    Fix eslintrc error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello from Tresor</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Hello from Tresor</h1>
+  </div>
+);
 
 export default App;
