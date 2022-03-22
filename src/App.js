0aa15a62cebad06b46040192ff6ac3eeb84b481b
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-22 23:20:54 +0200

    Import ChatEngine component in the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { ChatEngine } from 'react-chat-engine';
+
 const App = () => (
   <div className="App">
-    <h1>Hello from Tresor</h1>
+    <ChatEngine
+      height="100vh"
+      projectID="15922941-d4a7-4ad9-80af-140696e48ddd"
+      userName="tresorsawawa"
+      userSecret="123456"
+    />
   </div>
 );
 
